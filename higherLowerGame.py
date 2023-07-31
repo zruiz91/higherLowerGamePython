d93262f75dd2c2348c52503e269533c18dbe3d58
Zach Ruiz <powersbooth10@gmail.com>
2023-07-31 13:12:31 -0500

    added function that generates random choice of person from data arrary.

--- higherLowerGame.py
+++ higherLowerGame.py
@@ -4,12 +4,19 @@ import art
 import game_data
 
 
+def choose_random_person():
+  return random.choice(data)
+
 logo = art.logo
 data = game_data.data
 vs = art.vs
 
 print(logo)
 # print(data)
+
+person_a = choose_random_person()
+
+person_b = choose_random_person()
 
 print(f"Compare A: {person_b['name']}, a {person_b['description']}, from {person_b['country']}.")
 
