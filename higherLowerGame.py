033a0a669c86d2334ce200291024e02845817d59
Zach Ruiz <powersbooth10@gmail.com>
2023-07-31 13:11:27 -0500

    coded the starting print statments.

---
+++ higherLowerGame.py
@@ -0,0 +1,18 @@
+import random
+
+import art 
+import game_data
+
+
+logo = art.logo
+data = game_data.data
+vs = art.vs
+
+print(logo)
+# print(data)
+
+print(f"Compare A: {person_b['name']}, a {person_b['description']}, from {person_b['country']}.")
+
+print(vs)
+
+print(f"Compare B: {person_a['name']}, a {person_a['description']}, from {person_a['country']}.")
